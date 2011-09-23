17e450f05a3f9d24f703a479a64fe53b6a34e7b3
Christian Höltje <docwhat@gerf.org>
2011-09-23 12:24:33 -0400

    First working test.

--- lib/rspec/rails/example/widget_example_group.rb
+++ lib/rspec/rails/example/widget_example_group.rb
@@ -1,6 +1,8 @@
 module RSpec::Rails
   module WidgetExampleGroup
-    def render_widget
+    def render_widget(*args)
+      @controller = ::ActionController::Base.new
+      Apotomo::Widget.new(@controller, "root").render_widget(*args)
     end
   end
 end
--- spec/rspec-apotomo/widget_spec_spec.rb
+++ spec/rspec-apotomo/widget_spec_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
+require 'rails/all' # TODO This probably should be someplace else
 require 'rspec/rails/example/widget_example_group'
+require 'apotomo'
 
 module RSpec::Rails
   describe WidgetExampleGroup do
@@ -11,6 +13,22 @@ module RSpec::Rails
       # TODO use the better syntax when bug: https://github.com/rspec/rspec-core/issues/460
       # group.new.should respond_to(:render_widget)
       group.new.respond_to?(:render_widget).should be_true
+    end
+
+    context "as a user would use rspec-apotomo" do
+      include WidgetExampleGroup
+
+      it "calls render_widget in apotomo's widget'" do
+        ::Apotomo::Widget.any_instance.should_receive(:render_widget)
+        render_widget(:some_widget)
+      end
+
+      pending "sets response to the result of render_widget" do
+        ::Apotomo::Widget.any_instance.stub(:render_widget).and_return("expected string")
+        render_widget(:some_widget)
+        response.should == "expected string"
+      end
+
     end
   end
 end
