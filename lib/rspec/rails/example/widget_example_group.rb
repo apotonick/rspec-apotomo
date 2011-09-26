ee17fc0e21e937a3d0dc335775c4209fb460de87
Nick Sutterer <apotonick@gmail.com>
2011-09-26 10:51:47 +0200

    Using Apotomo::TestCase::TestMethods now. Still need to require apotomo-1.2.

--- lib/rspec/rails/example/widget_example_group.rb
+++ lib/rspec/rails/example/widget_example_group.rb
@@ -1,35 +1,17 @@
 require 'apotomo'
+require 'apotomo/test_case'
 
 module RSpec::Rails
   module WidgetExampleGroup
     extend ActiveSupport::Concern
-
+    
+    include Apotomo::TestCase::TestMethods
+    
     module InstanceMethods
-      # TODO documentation
-      def render_widget(*args)
-        @response = root.render_widget(*args)
-      end
-      
-      # TODO: extract TestMethods module in Apotomo core.
-      def trigger(type, source, *args)
-        source = root.find_widget(source)
-        source.fire(type, *args)
-        root.page_updates
-      end
-      
-
       # TODO documentation
       def parent_controller
         @controller = ::ActionController::Base.new
         @controller.extend Apotomo::Rails::ControllerMethods
-      end
-
-      # TODO documentation
-      def root
-        block = self.class.widgets_added or raise "Please setup a widget tree using has_widgets()"
-        @root ||= Apotomo::Widget.new(parent_controller, "root").tap do |root|
-          self.instance_exec(root, &block)
-        end
       end
 
       # TODO documentation
@@ -39,18 +21,5 @@ module RSpec::Rails
 
       include ::Apotomo::WidgetShortcuts
     end
-
-    module ClassMethods
-      # TODO documentation
-      def has_widgets(&blk)
-        @widgets_added = blk
-      end
-
-      # TODO documentation
-      def widgets_added
-        @widgets_added
-      end
-    end
-
   end
 end
