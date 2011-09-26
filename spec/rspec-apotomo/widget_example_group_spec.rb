87677f225b07d12eb38ff6d42987b640b87fe0f1
Christian Höltje <docwhat@gerf.org>
2011-09-26 10:59:57 -0400

    Reorganized tests for better understanding

--- spec/rspec-apotomo/widget_example_group_spec.rb
+++ spec/rspec-apotomo/widget_example_group_spec.rb
@@ -9,48 +9,71 @@ end
 
 module RSpec::Rails
   describe WidgetExampleGroup do
-    it "responds to render_widget" do
-      group = RSpec::Core::ExampleGroup.describe do
-        include WidgetExampleGroup
-      end
+    context "as RSpec" do
+      it "responds to render_widget" do
+        group = RSpec::Core::ExampleGroup.describe do
+          include WidgetExampleGroup
+        end
 
-      # Why is this weird? See https://github.com/rspec/rspec-core/issues/460
-      group.new.__should_for_example_group__ respond_to(:render_widget)
+        # Why is this weird? See https://github.com/rspec/rspec-core/issues/460
+        group.new.__should_for_example_group__ respond_to(:render_widget)
+      end
     end
 
-    context "as a user would use rspec-apotomo" do
+    context "as a test writer" do
       # A real user wouldn't use WidgetExampleGroup because
       # the rails rspec would include it automagically.
       include WidgetExampleGroup
 
-      # Tell Apotomo about our dummy widget
-      has_widgets do |root|
-        root << widget(:dummy)
+      context "- ::has_widget" do
+        has_widgets do |root|
+          root << widget(:dummy)
+        end
+
+        it "adds the widget to root" do
+          root.find_widget(:dummy).should_not be_nil
+        end
       end
 
-      it "calls render_widget in apotomo's widget'" do
-        ::Apotomo::Widget.any_instance.should_receive(:render_widget)
-        render_widget(:some_widget)
+      context "- #render_widget" do
+        has_widgets do |root|
+          root << widget(:dummy)
+        end
+
+        it "calls render_widget in apotomo's widget" do
+          ::Apotomo::Widget.any_instance.should_receive(:render_widget)
+          render_widget(:some_widget)
+        end
+
+        it "can use response to get the result of render_widget" do
+          ::Apotomo::Widget.any_instance.stub(:render_widget).and_return("expected string")
+          render_widget(:some_widget)
+          response.should == "expected string"
+        end
       end
 
-      it "can use response to get the result of render_widget" do
-        ::Apotomo::Widget.any_instance.stub(:render_widget).and_return("expected string")
-        render_widget(:some_widget)
-        response.should == "expected string"
+      context "- #trigger" do
+        has_widgets do |root|
+          root << widget(:dummy)
+        end
+
+        pending "triggers events" do
+          ::Apotomo::Widget.any_instance.stub(:doo).and_return("unexpected string")
+          trigger(:doo, :dummy)
+          response.should == "unexpected string"
+        end
+
+        pending "returns the page update array"
       end
 
-      it "can trigger events with #trigger and returns the page update array" do
-        ::Apotomo::Widget.any_instance.stub(:doo).and_return("unexpected string")
-        trigger(:doo, :dummy)
-        response.should == "unexpected string"
-      end
+      context "- #assign" do
+        has_widgets do |root|
+          root << widget(:dummy)
+        end
 
-      it "#has_widget added the widget to root" do
-        root.find_widget(:dummy).should_not be_nil
+        pending "gets the widget controller variables"
+        pending "sets the widget view variables"
       end
-
-      pending "can use assign() to get widget controller variables"
-      pending "can use assign() to set widget view variables"
     end
   end
 end
