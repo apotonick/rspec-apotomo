6df82bd836cfa3f2290bfea19349e7ba53a0a448
Nick Sutterer <apotonick@gmail.com>
2011-09-27 11:34:04 +0200

    added an UNTESTED generator.

---
+++ lib/generators/rspec/widget_generator.rb
@@ -0,0 +1,13 @@
+require 'generators/cells/base'
+
+module Rspec
+  module Generators
+    class WidgetGenerator < ::Cells::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
+
+      def create_cell_spec_file
+        template "widget_spec.erb", File.join("spec/widgets/#{file_name}_widget_spec.rb")
+      end
+    end
+  end
+end
