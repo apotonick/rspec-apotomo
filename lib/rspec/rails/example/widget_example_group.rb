346579700b98c937729095e4c621a45022ae067e
Nick Sutterer <apotonick@gmail.com>
2011-09-27 11:33:05 +0200

    setup a fake parent_controller in before so routes work.

--- lib/rspec/rails/example/widget_example_group.rb
+++ lib/rspec/rails/example/widget_example_group.rb
@@ -6,13 +6,26 @@ module RSpec::Rails
     extend ActiveSupport::Concern
 
     include Apotomo::TestCase::TestMethods
-
+    
+    included do
+      before do
+        @parent_controller = Class.new(ActionController::Base).new
+        @parent_controller.instance_eval do
+          extend Apotomo::Rails::ControllerMethods
+          def controller_path; 'barn'; end
+        end
+        
+        @parent_controller.request = ::ActionController::TestRequest.new
+      end
+    end
+    
+    
     module InstanceMethods
       # TODO documentation
       def response
         @last_invoke
       end
-
+      
       attr_reader :parent_controller
       include ::Apotomo::WidgetShortcuts
     end
